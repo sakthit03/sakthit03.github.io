49e904c9ff4b097db76ef17455adf55abf03d1fa
Sky Sakthi <sakthit03@gmail.com>
2021-01-14 20:37:07 +0530

    test

---
+++ footer.js
@@ -0,0 +1,4 @@
+document.writeln("<script type='text/javascript' src='assets/plugins/jquery/jquery-3.5.1.js'></script>");
+document.writeln("<script type='text/javascript' src='assets/plugins/bootstrap-3/js/bootstrap.min.js'></script>");
+document.writeln("<script type='text/javascript' src='assets/plugins/fontawesome-5/js/all.min.js'></script>");
+document.writeln("<script type='text/javascript' src='assets/js/custom.js'></script>");
